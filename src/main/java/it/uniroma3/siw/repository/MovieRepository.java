ab3c56be599161b6f21ea8962e313f697b4c13a0
SaverioPiana <pianasave@gmail.com>
2023-03-08 09:53:32 +0100

    init

---
+++ src/main/java/it/uniroma3/siw/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package it.uniroma3.siw.repository;
+
+import it.uniroma3.siw.model.Movie;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+    public List<Movie> findByYear(Integer year);
+    public boolean existsByTitleAndYear(String title, Integer year);
+}
